d5899b754cfe80a267366c1bc8c6a6129d046b58
yevhe <yevhe@YEVHENII>
2025-06-30 11:22:19 +0200

    basic program

--- test_file.py
+++ test_file.py
@@ -1,22 +1,20 @@
 from flask import Flask, request, jsonify
 import requests
 import os
-from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# Load your API key from environment variable
 API_KEY = os.getenv("CURRENCY_API_KEY")
 
 @app.route('/exchange', methods=['GET'])
 def get_exchange_rates():
     base = request.args.get("base", "USD")
-    currencies = request.args.get("currencies", "EUR,GBP,PLN,JPY")
+    currencies = request.args.get("currencies", "EUR,GBP,PLN,JPY,RUB,UAH")
 
     if not API_KEY:
         return jsonify({"error": "API key not set"}), 500
 
-    url = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}&base_currency=USD&currencies=EUR,GBP,PLN,JPY"
+    url = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}&base_currency={base}&currencies={currencies}"
     response = requests.get(url)
 
     if response.status_code != 200:
