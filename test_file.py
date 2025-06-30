0712a142f43cc8f1d2fc160fac38054001ead3fd
yevhe <yevhe@YEVHENII>
2025-06-30 11:38:52 +0200

    basic program

--- test_file.py
+++ test_file.py
@@ -9,12 +9,12 @@ API_KEY = os.getenv("CURRENCY_API_KEY")
 @app.route('/exchange', methods=['GET'])
 def get_exchange_rates():
     base = request.args.get("base", "USD")
-    currencies = request.args.get("currencies", "EUR,GBP,PLN,JPY,RUB")
+    currencies = request.args.get("currencies", "EUR,PLN,UAH,RUB")
 
     if not API_KEY:
         return jsonify({"error": "API key not set"}), 500
 
-    url = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}&base_currency={base}&currencies={currencies}"
+    url = f"https://api.forexrateapi.com/v1/latest?api_key={API_KEY}&base={base}&currencies={currencies}"
     response = requests.get(url)
 
     if response.status_code != 200:
